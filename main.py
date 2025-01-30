f631ed8ca18f4b1a212167e97a6e7054382d4134
pricer45 <robertprice45@gmail.com>
2025-01-30 01:11:44 +0000

    updated /w word and char count

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-print("hello world")
+def book_contents():
+    with open("books/frankenstein.txt", "r") as f:
+        file_contents = f.read()
+
+        print(file_contents)
+        
+
+
+def word_count():
+    number_of_words = 0
+
+    with open("books/frankenstein.txt", "r") as f:
+        file_contents = f.read()
+
+        lines = file_contents.split()
+        number_of_words += len(lines)
+        print(number_of_words)    
+
+def count_char():
+    with open("books/frankenstein.txt", "r") as f:
+        file_contents = f.read()
+
+        number_of_lines = 0
+        number_of_words = 0
+        number_of_chars = 0
+        char_count = {}
+
+        for line in file_contents:
+            number_of_lines += 1
+            words = line.split()
+            number_of_words += len(words)
+            number_of_chars += sum(len(word) for word in words)
+
+            for char in line:
+                char = char.lower()
+                if char in char_count:
+                    char_count[char] += 1
+                else:
+                    char_count[char] = 1
+            
+        #print(number_of_chars)
+        print(char_count)
+
+#book_contents()
+#word_count()
+count_char()
